bfcd2480147d1975a0f05bea33dbc8b009e70658
Ammarshepl <ammarshepl@gmail.com>
2025-09-03 13:10:05 +0300

    ADD models

---
+++ ITI Project/Models/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITI_Project.Models
+{
+	public class Stock
+	{
+		
+		
+			public int Id { get; set; }
+
+			[Required, ForeignKey("ShoppingCart")]
+			public int ShoppingCart_Id { get; set; }   
+			public ShoppingCart ShoppingCart { get; set; } 
+
+			[Required]
+			public int ProductId { get; set; }   
+			public Product Product { get; set; } 
+
+			[Required]
+			public int Quantity { get; set; }
+
+			[Required]
+			public double UnitPrice { get; set; }
+		
+	}
+}
