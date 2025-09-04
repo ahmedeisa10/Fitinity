cf90b6bb20b558c62148d9bcaaf64057400bf88e
gamalelgohary <gelgohary98@gmail.com>
2025-09-04 15:17:47 +0300

    Products

---
+++ ITI Project/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ITI_Project.Models;
+
+namespace ITI_Project.Repository
+{
+    public interface IProductRepository
+    {
+        Task AddProduct(Product product);
+        Task DeleteProduct(Product product);
+        Task<Product?> GetProductById(int id);
+        Task<IEnumerable<Product>> GetProducts();
+        Task UpdateProduct(Product product);
+    }
+}
